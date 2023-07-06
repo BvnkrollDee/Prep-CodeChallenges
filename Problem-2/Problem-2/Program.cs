21d4803294c32a7d73dc11e4023282f943f8cbee
Daron Faulkner Jr <dj200099@gmail.com>
2023-07-06 17:24:07 -0500

    problem 2 complete

---
+++ Problem-2/Problem-2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem_2;
+
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            int Year = 0;
+            Console.WriteLine("Type in a year and see if it's a leap year!!");
+            Year = Int32.Parse(Console.ReadLine());
+            if (Year % 4 == 0)
+            {
+                if (Year % 100 == 0 && Year % 400 == 0)
+                {
+
+                    Console.WriteLine("Hump Yearr!!!!");
+                }
+
+            }
+            else { Console.WriteLine("You've reached the mailbox of Uncle Rukus"); }
+
+        } while (true); {
+           
+
+        }
+        
+    }
+}
